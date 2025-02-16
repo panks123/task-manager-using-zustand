0459f4a2e6b91a054119772b5129c3faf44250a3
Pankaj <pankajkumarhzb787@gmail.com>
2025-02-16 17:31:04 +0530

    update

--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,8 @@ import { devtools, persist } from "zustand/middleware";
 const store = (set) => ({
     tasks: [],
     draggedTask: null,
-    addTask: (title, state) => set(store => ({ tasks: [...store.tasks, { title, state, id: new Date().getTime(), timestamp: new Date() }] }), false, 'addTask'),
+    addTask: (title, state) => set(store => ({ tasks: [...store.tasks, { title, state, id: new Date().getTime(), timestamp: new Date() }] }), false, 'addTask'), // the second argument in set() is a boolean (default value false) - specifying whether to replace the replace the entire state instead of merging with the existing state
+                                                                                                                                                                // Third argument is the action name that will be visible in the devtools 
     deleteTask: (id) => set(store => ({ tasks: store.tasks.filter(task => task.id !== id) }), false, 'deleteTask'),
     updateTask: (id, title) => set(store => ({ tasks: store.tasks.map(task => task.id === id ? { ...task, title } : task) }), false, 'updateTask'),
     setDraggedTask: (task) => set({ draggedTask: task }, false, 'setDraggedTask'),
